80a4e02144e1be14b41055714a1d38eb770d7110
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-03 21:18:20 +0530

    Variable js code

--- Chapter 1/script.js
+++ Chapter 1/script.js
@@ -1,3 +1,13 @@
 console.log("Hello There");
 console.log("Akhil Shetty M");
 console.log("I love javascript");
+
+
+//Variables
+fullName = "Akhil Shetty M";
+console.log(fullName);
+age = 22
+console.log(age);
+
+price = 99.99
+console.log(price);
