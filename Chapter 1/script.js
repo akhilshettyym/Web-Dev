cf8346f37db310dd1205afbf91fc07cbb2c7dc18
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-03 21:23:01 +0530

    Dynamically typed lang

--- Chapter 1/script.js
+++ Chapter 1/script.js
@@ -16,3 +16,15 @@ X = null;           //Represents an intentional empty or unknown value
 Y = undefined;      //Represents a missing or uninitialized value
 console.log(X);  
 console.log(Y);
+
+
+//Boolean type
+isFollow = false;
+console.log(isFollow);
+isFollow = true;
+console.log(isFollow);
+
+
+//Dynamically typed language
+// fullName = 25
+// console.log(fullName);
