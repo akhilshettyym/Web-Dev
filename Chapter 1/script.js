edd8e47ad0513afa59514507f0ebd26691ee65cc
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-04 11:20:38 +0530

    Keywords in js

--- Chapter 1/script.js
+++ Chapter 1/script.js
@@ -3,28 +3,35 @@ console.log("Akhil Shetty M");
 console.log("I love javascript");
 
 
-//Variables
-fullName = "Akhil Shetty M";
-console.log(fullName);
-age = 22
-console.log(age);
+// //Variables
+// fullName = "Akhil Shetty M";
+// console.log(fullName);
+// age = 22
+// console.log(age);
 
-price = 99.99
-console.log(price);
+// price = 99.99
+// console.log(price);
 
-X = null;           //Represents an intentional empty or unknown value
-Y = undefined;      //Represents a missing or uninitialized value
-console.log(X);  
-console.log(Y);
+// X = null;           //Represents an intentional empty or unknown value
+// Y = undefined;      //Represents a missing or uninitialized value
+// console.log(X);  
+// console.log(Y);
 
 
-//Boolean type
-isFollow = false;
-console.log(isFollow);
-isFollow = true;
-console.log(isFollow);
+// //Boolean type
+// isFollow = false;
+// console.log(isFollow);
+// isFollow = true;
+// console.log(isFollow);
 
 
 //Dynamically typed language
 // fullName = 25
 // console.log(fullName);
+
+
+// Keywords in JS
+let fullName = "Akhil Shetty M";
+let age = 22;
+let totalPrice = 1000;
+console.log(fullName);
