3efa229210e4d4be305967ffdf4c3afda8f3d224
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-04 11:38:29 +0530

    overall commit

--- Chapter 1/script.js
+++ Chapter 1/script.js
@@ -44,3 +44,25 @@ const age = 24;
 // age = 86;
 // age = 59;
 console.log(age);
+
+
+// Example 
+const PI = 3.14;
+console.log(PI);
+
+let a; //Just this will give error so
+a = 10
+console.log(a);
+
+const b = 10; // Declarations must be initialized like b = 10
+
+
+// This is a block this dosen't pose any error.
+{
+    let c = 20;
+    console.log(c);
+}
+{
+    let c = 30;
+    console.log(c);
+}
