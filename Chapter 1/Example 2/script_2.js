f6425c11a9c2e68a35958d1708bd9882776e44e5
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-04 21:03:00 +0530

    Example 2 - object profile

---
+++ Chapter 1/Example 2/script_2.js
@@ -0,0 +1,12 @@
+const profile = {
+    userName: "akhilshettym",
+    fullName: "Akhil Shetty M",
+    follers: 360,
+    following: 350,
+    isFollow: true,
+    post: 0,
+    bio: "I am HIM"
+};
+
+console.log(typeof profile)
+console.log(typeof profile["userName"])
