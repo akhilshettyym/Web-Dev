42f306afed074233ee9c941b520c493fdf1819eb
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-07 15:38:05 +0530

    Post and pre Inc Dec in javascript

--- Chapter 2/script.js
+++ Chapter 2/script.js
@@ -1,2 +1,18 @@
-console.log("Hello world !!!");
+//Arithmatic Operators
+let a = 5, b = 2;
+console.log("a =", a,"& b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
 
+
+//Unary Operator
+a = a + 1;
+console.log("a =", a);
+a++;
+console.log("a =", a);
+a--;
+console.log("a =", a);
