bbd8d1bc7be39ff08e5353f490c09d283aabaf85
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-19 12:55:19 +0530

    variables commit

---
+++ MeAndCode/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 124353
+let accountEmail = "akhilshettym2003@gmail.com"
+var accoundPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+accountID = 2  // Not allowed
+accountEmail = "hc@hc.com"
+accoundPassword = "323232"
+accountCity = "Mangaluru"
+console.log(accountID);
+console.log(accountEmail);
+console.log(accoundPassword);
+console.log(accountCity);
+console.table([accountID, accountEmail, accoundPassword, accountCity, accountState])
+
+/*
+Prefer not to use VAR
+*/
