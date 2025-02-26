61a3666ffd9defccb4a324c580bc785e9cb84405
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-26 17:35:17 +0530

    If statement

--- MeAndCode/04_ControlFlow/01_if.js
+++ MeAndCode/04_ControlFlow/01_if.js
@@ -1,42 +1,30 @@
-// If
-
-// const isUserLoggedIn = true; 
-// const temperature = 41;
-// if(temperature < 50){
-//     console.log("The user is logged in and the temperature is below 50");
-//     }else{
-//         console.log("The user is logged in and the temperature is above 50");
-// }
-// console.log("Executed");
-
-// const score = 200;
-// if(score >= 100){
-//     const power = "fly";
-//     console.log(`The user has a power of ${power}`);
-// }
-
+// User details
 const balance = 1000;
-// if (balance > 500) console.log("test");
-
-// if (balance < 500){
-//     console.log("Less than");
-// }else if (balance < 750){
-//     console.log("Less than 750");
-// }else if (balance < 900){
-//     console.log("Less than 900");
-// }else{
-//     console.log("Greater than 900");
-// }
-
 const isUserLoggedIn = true;
-const debitCard = true;
-const isUserLoggedInFromGoogle = false;
-const isUserLoggedInFromEmail = true;
+const hasDebitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
 
-if (isUserLoggedIn && debitCard){
-    console.log("User is logged in and has a debit card");
+// 1. Simplified conditional statement (Single-line if)
+if (balance > 500) console.log("User has sufficient balance.");
+
+// 2. Improved range checking using a single if-else structure
+if (balance < 500) {
+    console.log("Balance is less than 500.");
+} else if (balance < 750) {
+    console.log("Balance is less than 750.");
+} else if (balance < 900) {
+    console.log("Balance is less than 900.");
+} else {
+    console.log("Balance is greater than 900.");
 }
 
-if (isUserLoggedInFromGoogle || isUserLoggedInFromEmail){
-    console.log("User is logged in from Google or Email");
+// 3. Checking multiple conditions with AND (&&) for better readability
+if (isUserLoggedIn && hasDebitCard) {
+    console.log("User is logged in and has a debit card.");
+}
+
+// 4. Using OR (||) operator to check multiple login methods
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User is logged in from Google or Email.");
 }
