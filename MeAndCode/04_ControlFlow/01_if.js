4fe3206dbdf2fad078384409fc5502c4d56ad64a
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-26 17:34:08 +0530

    If statement

--- MeAndCode/04_ControlFlow/01_if.js
+++ MeAndCode/04_ControlFlow/01_if.js
@@ -9,8 +9,34 @@
 // }
 // console.log("Executed");
 
-const score = 200;
-if(score >= 100){
-    const power = "fly";
-    console.log(`The user has a power of ${power}`);
+// const score = 200;
+// if(score >= 100){
+//     const power = "fly";
+//     console.log(`The user has a power of ${power}`);
+// }
+
+const balance = 1000;
+// if (balance > 500) console.log("test");
+
+// if (balance < 500){
+//     console.log("Less than");
+// }else if (balance < 750){
+//     console.log("Less than 750");
+// }else if (balance < 900){
+//     console.log("Less than 900");
+// }else{
+//     console.log("Greater than 900");
+// }
+
+const isUserLoggedIn = true;
+const debitCard = true;
+const isUserLoggedInFromGoogle = false;
+const isUserLoggedInFromEmail = true;
+
+if (isUserLoggedIn && debitCard){
+    console.log("User is logged in and has a debit card");
+}
+
+if (isUserLoggedInFromGoogle || isUserLoggedInFromEmail){
+    console.log("User is logged in from Google or Email");
 }
