5b3511d76e511d7acf8a3d8bca782701522c248f
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-26 12:01:05 +0530

    If statement

---
+++ MeAndCode/04_ControlFlow/01_if.js
@@ -0,0 +1,16 @@
+// If
+
+// const isUserLoggedIn = true; 
+// const temperature = 41;
+// if(temperature < 50){
+//     console.log("The user is logged in and the temperature is below 50");
+//     }else{
+//         console.log("The user is logged in and the temperature is above 50");
+// }
+// console.log("Executed");
+
+const score = 200;
+if(score >= 100){
+    const power = "fly";
+    console.log(`The user has a power of ${power}`);
+}
